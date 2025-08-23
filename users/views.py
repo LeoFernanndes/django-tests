b7b742cd6056e5f759f529abd5d38c19a70d6ff6
leofernanndes <balbino.leonel@gmail.com>
2025-08-23 16:20:01 -0300

    feat: add basic pagination to user

--- users/views.py
+++ users/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets, permissions as drf_permissions
+from rest_framework import filters, viewsets, pagination, permissions as drf_permissions
 
 from users import models
 from users import permissions
 from users import serializers
 
 
+class DefaultPageNumberPaginationClass(pagination.PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class UserViewset(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     # is_staff is not available on list serializer but keeps being available on filtering
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]    
     filterset_fields = ['is_active', 'is_staff'] 
     ordering_fields = ['date_joined']
     ordering = ['-date_joined'] # overrides get_queryset ordering
+    pagination_class = DefaultPageNumberPaginationClass
     
     # TODO: check if there are better ways of setting permissions by action
     # TODO: check if this approach breaks any internal logic
