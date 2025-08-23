47c70683e1eaed98901dd2aa5ce717436a9255e7
leofernanndes <balbino.leonel@gmail.com>
2025-08-23 03:49:44 -0300

    feat: add basic granular permissions to users viewset

--- users/views.py
+++ users/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions as drf_permissions
 
 from users import models
+from users import permissions
 from users import serializers
 
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     
+    # TODO: check if there are better ways of setting permissions by action
+    # TODO: check if this approach breaks any internal logic
+    def get_permissions(self):
+        if self.action == 'create':
+            return [drf_permissions.AllowAny()]
+        if self.action == 'list':
+            return [drf_permissions.IsAdminUser()]
+        return [permissions.IsAdminOrSelf()]
+
     def get_serializer_class(self):
         if self.action == 'create':
             return serializers.UserCreateSerializer
