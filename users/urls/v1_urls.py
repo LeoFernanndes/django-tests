1f933c2a1945c6ff8dd6a5a5d3bb7146928e5839
leofernanndes <balbino.leonel@gmail.com>
2025-09-03 00:08:34 -0300

    fix: fix user self retrive and update endpoints

--- users/urls/v1_urls.py
+++ users/urls/v1_urls.py
@@ -6,11 +6,11 @@ from users.views import v1_views
 
 router = routers.DefaultRouter()
 router.register(r'', v1_views.UserViewset, basename='users') 
-router.register(f'/update-self', v1_views.UserUpdateSelf, basename='user-update-self')
+# router.register(f'update-self', v1_views.UserUpdateSelf, basename='user-update-self')
 
 
 urlpatterns = [
-    path('users', include(router.urls)),
+    path('users/', include(router.urls)),
 ]
 
 urlpatterns += router.urls
--- users/views/v1_views.py
+++ users/views/v1_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, pagination, permissions as drf_permissions, viewsets
+from rest_framework import decorators, filters, generics, mixins, pagination, response, permissions as drf_permissions, viewsets
 
 from users import models
 from users import permissions
@@ -27,8 +27,10 @@ class UserViewset(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == 'create':
             return [drf_permissions.AllowAny()]
-        if self.action == 'list':
+        elif self.action == 'list':
             return [drf_permissions.AllowAny()]
+        elif self.action in ('retrieve_self', 'update_self'):
+            return [drf_permissions.IsAuthenticated()]
         return [permissions.IsAdminOrSelf()]
 
     def get_serializer_class(self):
@@ -36,16 +38,30 @@ class UserViewset(viewsets.ModelViewSet):
             return v1_serializers.UserCreateSerializer
         elif self.action == 'update':
             return v1_serializers.UserUpdateSerializer
+        elif self.action == 'update_self':
+            return v1_serializers.UserUpdateSelfSerializer
         return v1_serializers.UserSerializer
 
     def get_queryset(self):
         return super().get_queryset().order_by('date_joined') # order is overriden by ordering but filter keep being properly applied
 
+    @decorators.action(detail=False, methods=['GET'], name='Retrieve self user')
+    def retrieve_self(self, request, *args, **kwargs):
+        instance = self.request.user
+        serializer = self.get_serializer(instance)
+        return response.Response(serializer.data)
 
-class UserUpdateSelf(mixins.UpdateModelMixin, viewsets.GenericViewSet):
-    queryset = models.User.objects.all()
-    serializer_class = v1_serializers.UserUpdateSelfSerializer
-    permission_classes = [permissions.SelfPermission]
+    @decorators.action(detail=False, methods=['POST'], name='Update self user')
+    def update_self(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
 
-    # def get_queryset(self):
-    #     return super().get_queryset().filter(id=self.request.user.id)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return response.Response(serializer.data)
