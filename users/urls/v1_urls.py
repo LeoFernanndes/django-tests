4d83ea2c0a1af2f239e9588636acc42b1a1915d2
leofernanndes <balbino.leonel@gmail.com>
2025-09-11 01:00:24 -0300

    fix: remove undesired extra /api/v1/ related to users endpoints

--- users/urls/v1_urls.py
+++ users/urls/v1_urls.py
@@ -6,11 +6,9 @@ from users.views import v1_views
 
 router = routers.DefaultRouter()
 router.register(r'', v1_views.UserViewset, basename='users') 
-# router.register(f'update-self', v1_views.UserUpdateSelf, basename='user-update-self')
 
 
 urlpatterns = [
     path('users/', include(router.urls)),
 ]
 
-urlpatterns += router.urls
